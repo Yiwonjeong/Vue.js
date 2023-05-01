976f2f873dd34c9ed1cea7a1e8979ab3f64b1615
이원정 <yiwonjeong@iwonjeong-ui-noteubug.local>
2023-05-01 18:39:19 +0900

    2023/04/03 Vue.js 소개

--- Ch01/src/App.vue
+++ Ch01/src/App.vue
@@ -5,22 +5,22 @@
 
 <script>
 /**
- * 날짜: 2023/04/03 이원정
+ * 날짜: 2023/04/03
  * 이름: 이원정
  * 내용: Ch01. Vue.js 소개
- * 
+ *
  * 개발환경 설정
- * - node.js 설치 
- * - VScode 설치 및 확장 플러그인(volar, vue3 snippets, prettier, ESLint) 설치 
+ * - node.js 설치
+ * - VScode 설치 및 확장 플러그인(volar, vue3 snippets, prettier, ESLint) 설치
  * - 크롬 확장 플러그인 (Vue.js devtools)
- * 
+ *
  * 프로젝트 생성/실행
  * - npm init vite-app Ch01
  * - cd Ch01
- * - npm install 
+ * - npm install
  * - npm run dev
  */
 export default {
-  name: 'App',
-}
+  name: "App",
+};
 </script>
